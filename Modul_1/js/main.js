05ea7ddf3827f67f9965f83674ab94a6faf25f72
nataliiasaltan <nataliiasaltan@gmail.com>
2020-03-13 14:03:58 +0200

    first

---
+++ Modul_1/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+       
+      
+     $('.icon-align-justify').click(function(){
+         $('.nav-list').slideToggle();
+     });
+    
+    });
+    
+     $(document).ready(function(){
+       $("#nav").on("click","a", function (event) {
+          
+           event.preventDefault();
+             
+           var id  = $(this).attr('href'),
+              
+            top = $(id).offset().top;
+                       
+           $('body,html').animate({scrollTop: top}, 1500);
+       });
+    });
